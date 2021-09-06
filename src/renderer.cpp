cf8ae884df25741fa12793f9f2e8e917158fb670
Ryan Sullivant <rsulli55@gmail.com>
2021-09-05 19:56:52 -0700

    Updated Renderer class and added implementations

---
+++ src/renderer.cpp
@@ -0,0 +1,101 @@
+#include "renderer.h"
+
+Renderer::Renderer(Shader& shader) : shader_{shader} {
+    init_texture();
+    init_vbo();
+    init_ebo();
+    init_vao();
+}
+
+Renderer::~Renderer() {
+    glDeleteTextures(1, &texture_);
+    glDeleteBuffers(1, &vbo_);
+    glDeleteBuffers(1, &ebo_);
+    glDeleteVertexArrays(1, &vao_);
+}
+
+void Renderer::render(const std::array<bool, Chip8::SCREEN_WIDTH * Chip8::SCREEN_HEIGHT>& screen) {
+    update_texture(screen);
+
+    // background color
+    glClearColor(bg_color_.r, bg_color_.g, bg_color_.b, 1.0f);
+    glClear(ClearBufferMask::GL_COLOR_BUFFER_BIT);
+
+    // bind vao, set box color uniform, and draw
+    glBindVertexArray(vao_);
+    shader_.set_float3("box_color", box_color_.r, box_color_.g, box_color_.b);
+    glDrawElements(GLenum::GL_TRIANGLES, NUM_ELEMENTS, GLenum::GL_UNSIGNED_INT, nullptr);
+}
+
+void Renderer::update_texture(const std::array<bool, Chip8::SCREEN_WIDTH * Chip8::SCREEN_HEIGHT>& screen) {
+    // calculate luminance from internal screen
+    auto to_luminance = [](bool b) { return b ? 255u : 0u; };
+    std::ranges::transform(screen, std::begin(luminance_), to_luminance);
+
+    // upload luminance to texture
+    glBindTexture(GLenum::GL_TEXTURE_2D, texture_);
+    glTexSubImage2D(GLenum::GL_TEXTURE_2D, 0, 0, 0, Chip8::SCREEN_WIDTH, Chip8::SCREEN_HEIGHT,
+            GLenum::GL_RED, GLenum::GL_UNSIGNED_BYTE, luminance_.data());
+}
+
+
+void Renderer::init_texture() {
+    glGenTextures(1, &texture_);
+
+    // use TEXTURE0
+    GLuint active_texture = 0;
+    glActiveTexture(GLenum::GL_TEXTURE0);
+    // configure texture
+    glBindTexture(GLenum::GL_TEXTURE_2D, texture_);
+    glTexParameteri(GLenum::GL_TEXTURE_2D, GLenum::GL_TEXTURE_MIN_FILTER, GLenum::GL_NEAREST);
+    glTexParameteri(GLenum::GL_TEXTURE_2D, GLenum::GL_TEXTURE_MAG_FILTER, GLenum::GL_NEAREST);
+    glTexParameteri(GLenum::GL_TEXTURE_2D, GLenum::GL_TEXTURE_WRAP_S, GLenum::GL_CLAMP_TO_EDGE);
+    glTexParameteri(GLenum::GL_TEXTURE_2D, GLenum::GL_TEXTURE_WRAP_T, GLenum::GL_CLAMP_TO_EDGE);
+
+    // create storage for texture
+    glTexStorage2D(GLenum::GL_TEXTURE_2D, 1, GLenum::GL_R8, Chip8::SCREEN_WIDTH, Chip8::SCREEN_HEIGHT);
+    // link uniform sampler to texture unit
+    // TODO: replace constant string "texture_sampler"
+    shader_.set_int("texture_sampler", active_texture);
+
+    // unbind texture
+    glBindTexture(GLenum::GL_TEXTURE_2D, 0);
+}
+
+void Renderer::init_vbo() {
+    glGenBuffers(1, &vbo_);
+    glBindBuffer(GLenum::GL_ARRAY_BUFFER, vbo_);
+    glBufferStorage(GLenum::GL_ARRAY_BUFFER, sizeof(vertices_), vertices_, 
+            BufferStorageMask::GL_NONE_BIT);
+    glBindBuffer(GLenum::GL_ARRAY_BUFFER, 0);
+}
+
+void Renderer::init_ebo() {
+    glGenBuffers(1, &ebo_);
+    glBindBuffer(GLenum::GL_ELEMENT_ARRAY_BUFFER, ebo_);
+    glBufferStorage(GLenum::GL_ELEMENT_ARRAY_BUFFER, sizeof(elements_), elements_, 
+            BufferStorageMask::GL_NONE_BIT);
+    glBindBuffer(GLenum::GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void Renderer::init_vao() {
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+    glBindBuffer(GLenum::GL_ARRAY_BUFFER, vbo_);
+    glBindBuffer(GLenum::GL_ELEMENT_ARRAY_BUFFER, ebo_);
+    // configure vao attributes
+    glVertexAttribPointer(POS_ATTR, POS_ATTR_SIZE, GLenum::GL_FLOAT, 
+            GL_FALSE, VERTEX_SIZE, reinterpret_cast<void*>(POS_OFFSET));
+    glVertexAttribPointer(TEX_ATTR, TEX_ATTR_SIZE, GLenum::GL_FLOAT, 
+            GL_FALSE, VERTEX_SIZE, reinterpret_cast<void*>(TEX_OFFSET));
+    // enable attributes
+    glEnableVertexAttribArray(POS_ATTR);
+    glEnableVertexAttribArray(TEX_ATTR);
+    // unbind everything
+    glBindBuffer(GLenum::GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GLenum::GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+
--- src/renderer.h
+++ src/renderer.h
@@ -5,15 +5,15 @@
 #include "common.h"
 #include "chip8.h"
 #include "rgb.h"
+#include "shader.h"
 
 using namespace gl;
 
 class Renderer {
 
     public:
-        Renderer() {};
-        // free resources
-        ~Renderer() {};
+        Renderer(Shader& shader);
+        ~Renderer();
         Renderer(const Renderer&) = delete;
         Renderer(Renderer&&) = delete;
         Renderer& operator=(const Renderer&) = delete;
@@ -26,30 +26,57 @@ class Renderer {
 
     private:
 
-      u32 screen_width_;
-      u32 screen_height_;
-      u32 box_size_;
+        // vertex array object, vertex buffer object, and elements buffer object
+        GLuint texture_;
+        GLuint vao_;
+        GLuint vbo_;
+        GLuint ebo_;
+        // TODO: maybe move shader to an argument of render, it is only used to set the sampler
+        // for texture
+        Shader& shader_;
+
+        bool show_debugger_ = true;
+
+      /* u32 screen_width_; */
+      /* u32 screen_height_; */
+      /* u32 box_size_; */
+      /* u32 frames_per_second_ = 60; */
+      /* u32 instructions_per_frame_ = 10; */
+
+        // colors
+        RGB bg_color_ {0.067f, 0.137f, 0.173f}; // Gunmetal
+        RGB box_color_ {0.867f, 0.867f, 0.867f}; // Gainsboro
+
+        std::array<GLubyte, Chip8::SCREEN_WIDTH * Chip8::SCREEN_HEIGHT> luminance_ {0u};
+
+        const u8 POS_ATTR = 0;
+        const u8 POS_ATTR_SIZE = 2;
+        const u8 POS_OFFSET = 0;
+        const u8 TEX_ATTR = 1;
+        const u8 TEX_ATTR_SIZE = 2;
+        const u8 TEX_OFFSET = 2 * sizeof(GLfloat);
+        const u32 VERTEX_SIZE = 4 * sizeof(GLfloat);
+        const u8 NUM_VERTICES = 16;
+        const GLfloat vertices_[16] = {
+            // screen coordinates   // texture coordinates
+            -1.0f,  -1.0f,          0.0f, 0.0f,      // Lower left corner
+             1.0f,  -1.0f,          1.0f, 0.0f,      // Lower right corner
+             1.0f,   1.0f,          1.0f, 1.0f,      // Upper right
+            -1.0f,   1.0f,          0.0f, 1.0f       // Upper left
+        };
+        const u8 NUM_ELEMENTS = 6;
+        const GLuint elements_[6] = {
+            0, 1, 2,
+            2, 3, 0
+        };
 
 
-      u32 frames_per_second_ = 60;
-      u32 instructions_per_frame_ = 10;
 
-      // colors
-      RGB bg_color_;
-      RGB box_color_;
 
-      // shaders
-      GLuint vert_shader_;
-      GLuint frag_shader_;
-      GLuint shader_prog_;
-
-      // vertex array object, vertex buffer object, and elements buffer object
-      GLuint vao_;
-      GLuint vbo_;
-      GLuint ebo_;
-
-      bool show_debugger_ = true;
-
-      void init_shaders();
+        void init_texture();
+        void init_vbo();
+        void init_ebo();
+        void init_vao();
+        void update_texture(const std::array<bool, Chip8::SCREEN_WIDTH * Chip8::SCREEN_HEIGHT>& screen);
 
 };
